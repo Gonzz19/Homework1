11e548fde5c290b464d128557f04d3e4ffcb872c
Gonzalo <gonzalomalaspina2005@gmail.com>
2025-03-19 20:37:44 +0000

    ej 1 con comentarios de que hice

--- ejercicio-1.cpp
+++ ejercicio-1.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
 #include <memory>
 
-using namespace std;
+using namespace std; //  Para evitar usar std:: en cada línea
 
+// Declaro las funciones que voy a usar
 unique_ptr< unique_ptr<int[]>[] > crearMatriz(int n);
+// Recibo la matriz por referencia para no hacer una copia
 void printMatriz(unique_ptr< unique_ptr<int[]>[] > &matriz, int n);
 
-int main() {
+int main() 
+{
+    // Pido el tamaño de la matriz
     int n;
     cout << "Ingrese el tamaño de la matriz: ";
     cin >> n;
 
+    // Creo la matriz definiendo el tipo de dato de salida
     unique_ptr< unique_ptr<int[]>[]> matriz = crearMatriz(n);
     printMatriz(matriz, n);
 
@@ -19,13 +24,16 @@ int main() {
 
 unique_ptr< unique_ptr<int[]>[] > crearMatriz(int n)
 {
+    // Creo la matriz de unique pointers 2D
     unique_ptr< unique_ptr<int[]>[] > matriz = make_unique< unique_ptr<int[]>[] >(n);
     int contador = 1;
     for (int i = 0; i < n; i++)
     {
+        // A cada fila le asigno un arreglo de enteros
         matriz[i] = make_unique<int[]>(n);
         for (int j = 0; j < n; j++)
         {
+            // lleno la matriz con numeros consecutivos
             matriz[i][j] = contador++;
         }
     }
@@ -35,8 +43,11 @@ unique_ptr< unique_ptr<int[]>[] > crearMatriz(int n)
 
 void printMatriz(unique_ptr< unique_ptr<int[]>[] > &matriz, int n)
 {
+    // Imprimo la matriz en orden inverso
     for (int i = n * n - 1; i >= 0; i--)
     {
+        // Calculo los indices de la matriz
+        // Utilizo los int para que me trunque los decimales
         int k =  i / n;
         int j = i % n;
         cout << "Matriz[" << k << "][" << j << "] = " << matriz[k][j] << endl;
